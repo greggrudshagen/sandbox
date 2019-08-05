e6ce0bbad4629f0865d66f9f1e4aafeabc718342
Gregg Rudshagen <gregg.rudshagen@gmail.com>
2019-08-05 13:41:42 -0700

    Created a FizzBuzz directory and module files.

---
+++ src/fizzbuzz/fizzbuzz.c
@@ -0,0 +1,18 @@
+#include <fizzbuzz.h>
+
+#include <stdio.h>
+
+int32_t fizzbuzz_main()
+{
+    int32_t rc = 0;
+    printf("%s\n", __FUNCTION__);
+
+#if 1
+
+#elif 0
+
+#else
+
+#endif
+    return rc;
+}
